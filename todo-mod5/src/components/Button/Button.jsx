857348864993d66ff460bfe092c6aee611f3fce2
Carol Saint <santocarolina98@gmail.com>
2022-07-26 15:24:25 -0300

    components files created, header implemented

---
+++ todo-mod5/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Button.module.css'
+
+const Button = () => {
+  return (
+    <button></button>
+  )
+}
+
+export default Button
---
+++ todo-mod5/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={style.header}>
+
+        <div>
+            <button>a</button>
+            <button>a</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
